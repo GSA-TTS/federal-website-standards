78850710865f629eb5a831300f9e5aa71af80284
Caley Woods <caley.woods@gsa.gov>
2024-09-12 09:08:34 -0500

    Add external link icon class to all links with a TLD other than .gov (#195)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 require('@uswds/uswds');
+
+// Append the USWDS 'usa-link--external' class to any link whose TLD is not .gov
+document.addEventListener('DOMContentLoaded', () => {
+  Array.from(document.querySelectorAll('a[href]'))
+    // Filter out localhost and links to targets in the same page
+    .filter(({ href }) => !['localhost', ''].includes(new URL(href).hostname))
+    // Filter out .gov and .mil links
+    .filter(({ href }) => !/\.(gov|mil)$/i.test(new URL(href).hostname))
+    // Filter out social and logo links
+    .filter(
+      (anchor) =>
+        !['usa-social-link', 'usa-identifier__logo'].some((c) =>
+          anchor.className.includes(c)
+        )
+    )
+    // For all the remaining anchors, add the external link class
+    .forEach((anchor) => {
+      anchor.className = [anchor.className, 'usa-link--external'].join(' ')
+    })
+});
