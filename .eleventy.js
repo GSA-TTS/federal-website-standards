6faf5b4034384f695e422cfff365a3c99ad8cf1c
Michelle Rago <60157596+michelle-rago@users.noreply.github.com>
2024-10-18 09:30:00 -0400

    Add favicon references

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,10 @@ module.exports = function (config) {
   // Copy the `admin` folders to the output
   config.addPassthroughCopy('admin');
 
+  // Copy the favicon
+  config.addPassthroughCopy({
+    "_img/favicon/": "_img/favicon/"});
+
   // Copy USWDS init JS so we can load it in HEAD to prevent banner flashing
   config.addPassthroughCopy({'./node_modules/@uswds/uswds/dist/js/uswds-init.js': 'assets/js/uswds-init.js'});
 
