dc03dc653fe57481ce856569505f9117415a42c3
Caley Woods <caley.woods@gsa.gov>
2024-11-01 10:37:07 -0500

    Re-add purgecss and fix federalist build script
    PurgeCSS is working now; the `federalist` build script was missing the `assets:autoprefix` line that kicks off postcss and purgecss.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
+const purgeCSSOptions = require('./config/purgecss-options');
+
 module.exports = {
   plugins: [
     require('autoprefixer')({
       map: process.env.ELEVENTY_ENV !== 'production'
     }),
+    ...(process.env.ELEVENTY_ENV === 'production' ? [require("@fullhuman/postcss-purgecss")(purgeCSSOptions)] : [])
   ],
 };
